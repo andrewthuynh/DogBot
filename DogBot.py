8a04de0e99bdf480987782e91c1aa5f3cd66b882
ahuynh <ath28644@uga.edu>
2018-01-24 21:26:10 -0500

    First commit

---
+++ DogBot.py
@@ -0,0 +1,37 @@
+#DogBot
+#Written by Andrew Huynh
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot 
+import asyncio
+import random
+
+dogs = ['1.jpeg', '2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg','9.jpeg','10.jpeg']
+
+client = discord.Client()
+server = client.get_server('405860470252240916')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+	print('bot is ready')
+
+@bot.command(pass_context=True)
+async def bark(ctx):
+	await bot.say("woof woof")
+	print('user barked')
+
+@bot.command(pass_context=True)
+async def dog(ctx):
+	ran = dogs[random.randrange(0,9,1)]
+	await bot.send_file(ctx.message.channel, ran)
+	print('dog sent')
+
+@bot.command(pass_context=True)
+async def commands(ctx):
+	await bot.say('!dog\n!bark')
+
+
+bot.run("NDA1ODYwMDI2OTAxNzI1MTg0.DUqktA.dazUSvu1qxJ6THe-1_SWbd-2Btk")
